1415391a3f50ca78fff1b7fb59b48a15fa12e11e
gvidas333 <gvidas.bac@gmail.com>
2023-10-03 23:46:06 +0300

    Added add calendar button feature

--- WakeyWakey/MainPage.xaml.cs
+++ WakeyWakey/MainPage.xaml.cs
@@ -2,24 +2,59 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    private async void OnUploadButtonClick(object sender, EventArgs e)
+    {
+        try
+        {
+            // Use the FilePicker to select a calendar file
+            var fileResult = await FilePicker.PickAsync(new PickOptions
+            {
+                FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+                {
+                    { DevicePlatform.MacCatalyst, new[] { "ics" } }
+                })
+            });
+
+
+            if (fileResult != null)
+            {
+                // Process the selected file (e.g., save it, parse it, etc.)
+                // Example: var stream = await fileResult.OpenReadAsync();
+                var stream = await fileResult.OpenReadAsync();
+                var fileName = fileResult.FileName;
+                var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), fileName);
+
+                using (var fileStream = File.Create(filePath))
+                {
+                    await stream.CopyToAsync(fileStream);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle any exceptions
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
 }
 
 
