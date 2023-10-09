db0ae0e42057aa840ec6e5f5dbe1eacfca4d586f
MrRobot2002 <just.zubavicius@gmail.com>
2023-10-09 19:25:27 +0300

    opa, modeliai, netiketa

---
+++ WakeyWakey/Models/AssignmentModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace WakeyWakey.WakeyWakey.core.Models
+{
+    public class Assignment
+    {
+        public DateTime DeadlineDate { get; set; }
+
+        public int AssignmentID { get; set; }
+
+        public int SubjectID { get; set; }
+
+        public int ParentID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime EstimatedDuration { get; set; }
+
+        public DateTime OverallDuration { get; set; }
+
+    }
+}
+
